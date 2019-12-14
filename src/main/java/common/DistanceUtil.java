94f2111b873c744707814b82a3d4ba77dedbebe4
DriesClaesenPXL <dries.claesen@student.pxl.be>
2019-12-14 17:40:36 +0100

    DistanceUtil

--- src/main/java/common/DistanceUtil.java
+++ src/main/java/common/DistanceUtil.java
@@ -1,12 +1,26 @@
 package common;
+import city.City;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Set;
 
 public class DistanceUtil {
-    public static <T> T findClosest (Set<T> elements, T otherElement){
-        for (T element : elements){
+    public static <T extends City> T findClosest (Set<T> elements, T otherElement){
+        ArrayList<Double> distances = new ArrayList<Double>();
+        T shortestCity = null;
 
+        for (T element : elements){
+            distances.add(element.distance(otherElement));
         }
-        return null;
+
+        Collections.sort(distances);
+
+        for (T element : elements) {
+            if (element.distance(otherElement) == distances.get(0)) {
+                shortestCity = element;
+            }
+        }
+        return shortestCity;
     }
 }
